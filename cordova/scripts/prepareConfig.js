a3bbd0a0a749ce64dcaddfae1072718aaed2c504
Tuvshinbayar <themonmail@gmail.com>
2018-02-15 10:23:09 +0800

    updated ip configuration

--- cordova/scripts/prepareConfig.js
+++ cordova/scripts/prepareConfig.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var path = require('path');
 
 var dir = '../'
-var configFile = fs.readFileSync(dir + 'cordova/config.xml');
+var configFile = fs.readFileSync(dir + 'cordova/config-live.xml');
 var envFile = fs.readFileSync(dir + '.env').toString();
 
 var HOST = '';
@@ -10,9 +10,9 @@ var config = configFile.toString();
 
 var PACKAGE = envFile.substring(8, 12);
 if(PACKAGE == 'fals'){
-	HOST = envFile.substring(envFile.lastIndexOf('=')+1);
+	HOST = envFile.substring(envFile.lastIndexOf('=')+1)+'/';
 }
 
-var content = config.replace(new RegExp('(?<=<content src=")(.*)(?=" />)'), HOST+'/index.html');
+var content = config.replace(new RegExp('(?<=<content src=")(.*)(?=" />)'), HOST+'index.html');
 //console.log(content);
 fs.writeFileSync(dir + 'cordova/config.xml', content);
